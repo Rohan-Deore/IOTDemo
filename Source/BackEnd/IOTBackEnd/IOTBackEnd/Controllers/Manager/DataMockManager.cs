96042a76253a7f26c16e72148e6eee06de037f0a
Rohan-Deore <deorerohan.2612@gmail.com>
2025-03-01 13:51:10 +0530

    Database manager added

--- Source/BackEnd/IOTBackEnd/IOTBackEnd/Controllers/Manager/DataMockManager.cs
+++ Source/BackEnd/IOTBackEnd/IOTBackEnd/Controllers/Manager/DataMockManager.cs
@@ -4,14 +4,39 @@
     {
         public IEnumerable<DeviceStatus> GetDeviceStatus()
         {
-            return Enumerable.Range(1, 5).Select(index => new DeviceStatus
+            return GetDeviceStatus();
+        }
+
+        public List<DeviceStatus> CreateData(string name)
+        {
+            DateTime statusTime = new DateTime(2025, 3, 1, 8, 0, 0);
+            TimeSpan delta = TimeSpan.FromMinutes(1);
+            List<DeviceStatus> statuses = new List<DeviceStatus>();
+
+            DateTime limit = new DateTime(2025, 3, 1, 18, 0, 0);
+            bool currentStatus = true;
+
+            Random random = new Random();
+
+            while (statusTime < limit)
             {
-                StatusTime = DateTime.Now.AddDays(index),
-                Status = true,
-                DeviceName = "Raspberry Pi",
-                DeviceVersion = "1.0"
-            })
-            .ToArray();
+                int sameStatus = random.Next(7, 20);
+                for (int x = 0; x < sameStatus; x++)
+                {
+                    statusTime = statusTime.Add(delta);
+                    statuses.Add(new DeviceStatus() 
+                    { 
+                        DeviceName = "Raspberry pi", 
+                        Status = currentStatus, 
+                        StatusTime = statusTime, 
+                        DeviceVersion = "1.0" 
+                    });
+                }
+
+                currentStatus = !currentStatus;
+            }
+
+            return statuses;
         }
     }
 }
---
+++ Source/BackEnd/IOTBackEnd/IOTBackEnd/Controllers/Manager/DatabaseManager.cs
@@ -0,0 +1,12 @@
+﻿namespace IOTBackEnd.Controllers.Manager
+{
+    public class DatabaseManager : IManager
+    {
+        public DatabaseManager() { }
+
+        public IEnumerable<DeviceStatus> GetDeviceStatus()
+        {
+            return new List<DeviceStatus>();
+        }
+    }
+}
