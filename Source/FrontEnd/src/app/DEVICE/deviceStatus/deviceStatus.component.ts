6d9b8d44c27ad426919ef36785e09a563e37cae9
devendra <devnahire@gmail.com>
2025-03-03 15:34:57 +0530

    Front end code initialised

---
+++ Source/FrontEnd/src/app/DEVICE/deviceStatus/deviceStatus.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deviceStatus',
+  templateUrl: './deviceStatus.component.html',
+  styleUrls: ['./deviceStatus.component.css']
+})
+export class DeviceStatusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ Source/FrontEnd/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GraphComponent } from "./DEVICE/Graph/graph.component";
+import { DeviceStatusComponent } from "./DEVICE/DeviceStatus/DeviceStatus.component";
+import { NgApexchartsModule } from "ng-apexcharts";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    GraphComponent,DeviceStatusComponent,NgApexchartsModule
+],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
